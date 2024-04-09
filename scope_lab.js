b842144b24b2ce01bd86039beb9ecca302d4596c
El Mustapha Belegchour <elmustaphabelegchour@gmail.com>
2024-04-09 10:22:03 -0400

    reorganisation de fichier

---
+++ scope_lab.js
@@ -0,0 +1,12 @@
+// Global scope
+
+var globalVar = " Je suis une varbiable globale ";
+let globalLet = " Je suis globale aussi maivs scoped avec let";
+const globaLConst = "je suis une constante globale";
+
+{
+// block scope
+var blockVar = "Je suis une block-scoped var";
+let blockLet = "Je suis une block-scoped let";
+const blockConst = "Je suis une block-scoped constante"
+}
